d452d643f2747f3e57ea5e66b51ff83c679677e3
Pakteema <sand_mocha@hotmail.com>
2017-11-19 15:59:40 +0700

    all

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './pages/Login';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
+import './styles/semantic.min.css'
+
+
+
+ReactDOM.render((
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  ), document.getElementById('root'))
+registerServiceWorker()
